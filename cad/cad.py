695658af0666d03b4f4887b5e4a2ba1d07d8be4f
quanturtle <alvaromachucar@gmail.com>
2025-01-23 11:07:35 -0300

    fixed cad pipeline

--- cad/cad.py
+++ cad/cad.py
@@ -4,9 +4,9 @@ from build123d import *
 
 def generate_cad(pts: List, name: str):
     with BuildPart() as part:
-        with BuildSketch() as s:
+        with BuildSketch(Plane.XZ) as s:
             with BuildLine() as l:
-                Polyline(*pts, close=True)
+                l1 = Polyline(*pts)
             
             trace(line_width=2.5)
         
