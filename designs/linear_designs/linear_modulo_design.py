27ab1e1dfc5874d1d1f8999b3d8652657dde9a34
quanturtle <alvaromachucar@gmail.com>
2025-04-04 11:17:03 -0300

    added some linear designs

--- designs/linear_designs/linear_modulo_design.py
+++ designs/linear_designs/linear_modulo_design.py
@@ -1,20 +1,20 @@
 from shapes import draw_linear_modulo
 
-#TODO
+
 def design_105():
     draw_linear_modulo()
     
 
 def design_106():
-    draw_linear_modulo()
+    draw_linear_modulo(N=500, M=500, K1=11/7, K2=7/3, H=3)
     
 
 def design_107():
-    draw_linear_modulo()
+    draw_linear_modulo(N=400, M=400, K1=4, K2=2, H=2)
     
     
 def design_108():
-    draw_linear_modulo()
+    draw_linear_modulo(N=300, M=300, K1=5, K2=3, H=2)
 
     
 def design_109():
